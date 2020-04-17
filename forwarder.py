6b1e572c3230aaa794e9150318b1681c5978d848
jestape <jestape@protonmail.com>
2020-04-17 17:53:17 +0200

    add redundant db

--- forwarder.py
+++ forwarder.py
@@ -4,11 +4,22 @@ import sys
 import os
 import json
 import serial
+
 import time
+from datetime import datetime
+
 import subprocess
 import requests
+import mysql.connector
 
 print("\nSetting up enviroment for vinduinos and weather station")
+
+db = mysql.connector.connect(
+  host=os.getenv("DATABASE_HOST", default = 'localhost') + ":" + os.getenv("DATABASE_PORT", default = '3306')  ,
+  user=os.getenv("DATABASE_USERNAME", default = 'test'),
+  passwd=os.getenv("DATABASE_PASSWORD", default = 'test123),
+  database=os.getenv("DATABASE_NAME", default = 'test_db')
+)
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=900)
 
@@ -43,16 +54,23 @@ while True:
   if(splitted_line[1] == "XBA5TPH2RV65Q52K"):
     data.update({"sensor_id": "malbec-oeste"})
 
-  data.update({"sensor2": float(splitted_line[2])})
-  data.update({"sensor1": float(splitted_line[3])})
-  data.update({"sensor05": float(splitted_line[4])})
-  data.update({"sensor005": float(splitted_line[5])})
-  data.update({"timestamp": int(time.time())})
-
-  data.update({"hash": str(time.time())})
+  data.update({"humidity2": float(splitted_line[2])})
+  data.update({"humidity1": float(splitted_line[3])})
+  data.update({"humidity05": float(splitted_line[4])})
+  data.update({"humidity005": float(splitted_line[5])})
+  data.update({"timestamp": datetime.fromtimestamp(time.time()).strftime("%Y-%m-%dT%H:%M:%S.00Z")})
+  data.update({"hash": str(time.time())}) #temporal
 
   url = os.getenv("API_ENDPOINT", default = 'https://localhost:4040') + "/sensor_data"
-  x = requests.post(url, data = data)
+  res = requests.post(url, data = data)
+
+  if res.status_code == 200:
+
+    cursor = db.cursor()
+    sql = "INSERT INTO sensor_data (wind_velocity, wind_gust, wind_direction, pressure, rain, temperature, humidity, sensor_id, humidity2, humidity1, humidity05, humidity005, timestamp, hash) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = tuple([value for _, value in data.items()])
+    cursor.execute(sql, val)
+    db.commit()
 
   print("Line was read and preprocessed: " + json.dumps(data))
 
--- test.py
+++ test.py
@@ -1,9 +1,33 @@
 import json
+import requests
+import os
+import time
+from datetime import datetime
 
-process = b'{"wind_velocity":0.0,"wind_gust":0.0,"wind_direction":0.0,"pressure":90.897,"rain":0.0}'
+db = mysql.connector.connect(
+  host=os.getenv("DATABASE_HOST", default = 'database') + ":" + os.getenv("DATABASE_PORT", default = '3306')  ,
+  user=os.getenv("DATABASE_USERNAME", default = 'test'),
+  passwd=os.getenv("DATABASE_PASSWORD", default = 'test123),
+  database=os.getenv("DATABASE_NAME", default = 'test_db')
+)
+
+process = b'{"wind_velocity": 0.0, "wind_gust": 0.0, "wind_direction": 0.0, "pressure": 90.932, "rain": 0.0, "temperature": null, "humidity": null, "sensor_id": "malbec-oeste", "sensor2": 13.0, "sensor1": 16.0, "sensor05": 9.0, "sensor005": 11.0, "hash": "15871252792.51475"}'
 weather_station_info = process.decode("utf-8")
-print(weather_station_info)
 data = json.loads(weather_station_info)
+data.update({"timestamp": datetime.fromtimestamp(time.time()).strftime("%Y-%m-%dT%H:%M:%S.00Z")})
+print(data)
 
-data.update({"sensor2": 3.7})
-print(json.dumps(data))
+url = os.getenv("API_ENDPOINT", 
+                default = 'http://10.112.48.25:4040') + "/sensor_data"
+res = requests.post(url, json = data,
+                headers= {'Content-type': 'application/json'})
+
+if res.status_code == 200:
+
+    cursor = db.cursor()
+    sql = "INSERT INTO sensor_data (wind_velocity, wind_gust, wind_direction, pressure, rain, temperature, humidity, sensor_id, humidity2, humidity1, humidity05, humidity005, timestamp, hash) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = tuple([value for _, value in data.items()])
+    cursor.execute(sql, val)
+    db.commit()
+
+print(x.content)
